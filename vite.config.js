61423edc6995c7bb2fd2c2520013d1c18909baf1
SamiJelassi <samijlassi2909@gmail.com>
2026-03-26 17:20:47 +0100

    Setup Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild', // Use esbuild instead of terser
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'react-vendor'
-            }
-            if (id.includes('@mui') || id.includes('@emotion')) {
-              return 'mui-vendor'
-            }
-            if (id.includes('leaflet')) {
-              return 'map-vendor'
-            }
-            return 'vendor'
-          }
-        }
-      }
-    }
-  }
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
 })
